06a1a64326bf07428594b329522db842f08e331f
ThaGary <garyanderson31591@gmail.com>
2018-12-19 15:18:58 -0700

    fixed spotify ids in seeds

--- migrations/20181218110719_user_music.js
+++ migrations/20181218110719_user_music.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('user_music', (user_music) => {
          user_music.increments('id')
          user_music.integer('user_id').references('id').inTable('users')
-         user_music.sting('spotify_id').references('spotify_id').inTable('music')
+         user_music.string('spotify_id').references('spotify_id').inTable('music')
     });
 };
 
