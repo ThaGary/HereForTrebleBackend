87bf6a1b6c9b98c32a71630e55f8c7de8531936b
ThaGary <garyanderson31591@gmail.com>
2018-12-19 15:13:59 -0700

    fixed spotify ids in seeds

--- seeds/03_user_music.js
+++ seeds/03_user_music.js
@@ -7,35 +7,35 @@ exports.seed = function(knex, Promise) {
       return knex('user_music').insert([
         {
           user_id: 1,
-          music_id: 1
+          spotify_id: "06HL4z0CvFAxyc27GXpf02",
         },
         {
           user_id: 2,
-          music_id: 2
+          spotify_id: "6vWDO969PvNqNYHIOW5v0m",
         },
         {
           user_id: 3,
-          music_id: 3
+          spotify_id: "1Ffb6ejR6Fe5IamqA5oRUF"
         },
         {
           user_id: 4,
-          music_id: 4
+          spotify_id: "2fSaE6BXtQy0x7R7v9IOmZ",
         },
         {
           user_id: 2,
-          music_id: 1
+          spotify_id: "06HL4z0CvFAxyc27GXpf02",
         },
         {
           user_id: 3,
-          music_id: 2
+          spotify_id: "6vWDO969PvNqNYHIOW5v0m",
         },
         {
           user_id: 4,
-          music_id: 3
+          spotify_id: "1Ffb6ejR6Fe5IamqA5oRUF"
         },
         {
           user_id: 1,
-          music_id: 4
+          spotify_id: "2fSaE6BXtQy0x7R7v9IOmZ",
         }
       ]);
     });
